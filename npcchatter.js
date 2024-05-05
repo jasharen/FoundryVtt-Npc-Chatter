8c8cd63ba39b866ee01b330ff6f3333298a83189
Gerhardus <gbadenhorst2@gmail.com>
2024-05-05 17:33:48 +0200

    updated: fixed for v11

--- npcchatter.js
+++ npcchatter.js
@@ -2,91 +2,115 @@ class NpcChatter {
   
   static timer;
 
+  getRandomIndex(iterable) {
+    return Math.floor((Math.random() * iterable.length));
+  }
+
   getChatterTables() {
     const chatterFolder = game.folders.contents.find(x => x.type == "RollTable" && x.name.toLowerCase() == "npc chatter");
-    if ( !chatterFolder ) {
+
+    if (!chatterFolder) {
       ui.notifications.warn("Could not find the 'NPC Chatter' Folder");
       return [];
     }
-    const tables = game.tables.contents.filter(x => x.name.toLowerCase().endsWith("chatter") || x.data.folder == chatterFolder.id);
-    return tables;
+
+    return chatterFolder.contents;
   }
 
-  randomGlobalChatterEvery(milliseconds, options={}) {
-    NpcChatter.timer = window.setInterval(() => { game.npcChatter.globalChatter(options); }, milliseconds);
+  randomGlobalChatterEvery(milliseconds, options = {}) {
+    NpcChatter.timer = window.setInterval(() => { 
+      game.npcChatter.globalChatter(options); 
+    }, milliseconds);
   }
 
   static _getChatterScene() {
     const sceneType = game.settings.get("npc-chatter", "scenetype");
     switch (sceneType) {
-      case "active": return game.scenes.find(x => x.active);
-      case "viewed": return game.scenes.find(x => x.id === game.user.viewedScene);
+      case "active": return game.scenes.active;
+      case "viewed": return game.scenes.get(game.user.viewedScene);
     }
   }
 
-  async globalChatter(options={}) {
-    const tables = this.getChatterTables();
-
-    const userCharacterActorIds = game.users.contents.filter(x => x.character).map(x => x.character.id);
+  async globalChatter(options = {}) {
+    const chatterTables = this.getChatterTables();
     const scene = NpcChatter._getChatterScene();
-    const npcTokens = scene.data.tokens.filter(x => !userCharacterActorIds.includes(x.actorId));
 
-    const eligibleTables = tables.filter(x => npcTokens.filter(t => x.name.toLowerCase().includes(t.name.toLowerCase().replace("chatter", "").trim()) > 0));
-    if ( eligibleTables.length === 0 ) {
-      ui.notifications.warn("You have no NPC Chatter tables setup for these tokens");
+    if (!scene) {
+      ui.notifications.warn("No valid scene found for NPC chatter.");
       return;
     }
 
-    const tableIndex = Math.floor((Math.random() * eligibleTables.length) + 0);
-    const table = eligibleTables[tableIndex];
+    const npcTokenNames = new Set(scene.tokens.contents.map(t => t.name.toLowerCase()));
 
-    const eligableTokens = npcTokens.filter(x => x.name.toLowerCase().includes(table.name.toLowerCase().replace("chatter", "").trim()));
+    const eligibleTables = chatterTables.filter(t => npcTokenNames.has(t.name.toLowerCase().replace("chatter", "").trim()));
 
-    const tokenIndex = Math.floor((Math.random() * eligableTokens.length) + 0);
-    const token = eligableTokens[tokenIndex];
+    if (!eligibleTables.length) {
+      ui.notifications.warn("You have no NPC Chatter tables set up for these tokens");
+      return;
+    }
 
-    if (token == undefined) return;
-
-
-    let roll = await table.roll();
-    const result = roll.results[0].data.text;
+    const table = eligibleTables[this.getRandomIndex(eligibleTables)];
+    const eligibleTokens = scene.tokens.contents.filter(
+      x => x.name.toLowerCase().includes(table.name.toLowerCase().replace("chatter", "").trim())
+    );
+    const tokenDocument = eligibleTokens[this.getRandomIndex(eligibleTokens)];
+    if (!tokenDocument) {
+      console.error("No token document found for chatter. Aborting...");
+      return;
+    }
+    const token = canvas.tokens.placeables.find(t => t.id === tokenDocument.id);
+    if (!token) {
+      console.error("Token object not found on the canvas for the ID: " + tokenDocument.id);
+      return;
+    }
+    const roll = await table.roll();
+    const result = roll.results[0].text;
     game.socket.emit("module.npc-chatter", {
-      tokenId: token.data._id,
+      tokenId: token.id,
       msg: result
     });
     const emote = Object.keys(options).length ? {emote: options} : false;
-    await canvas.hud.bubbles.say(token.data, result, emote);
+    if (token.isVisible) {
+      try {
+        await canvas.hud.bubbles.say(token, result, emote);
+      } catch (error) {
+        console.error("Failed to create chat bubble:", error);
+      }
+    } else {
+    }
   }
 
-  async tokenChatter(token, options={}) {
-    if ( !token ) {
+  async tokenChatter(token, options = {}) {
+    //console.log("Initiating chatter for a single token", token);
+    if (!token) {
       ui.notifications.error("No Token passed in");
       return;
     }
-    const tables = this.getChatterTables();
 
+    const tables = this.getChatterTables();
     const eligibleTables = tables.filter(x => token.name.toLowerCase().includes(x.name.toLowerCase().replace("chatter", "").trim()));
-    if ( eligibleTables.length === 0 ) {
+    if (!eligibleTables.length) {
       ui.notifications.warn("You have no NPC Chatter tables setup for this token");
       return;
     }
 
-    const tableIndex = Math.floor((Math.random() * eligibleTables.length) + 0);
+    const tableIndex = this.getRandomIndex(eligibleTables);
     const table = eligibleTables[tableIndex];
+
     let roll = await table.roll();
-    const result = roll.results[0].data.text;
+    const result = roll.results[0].text;
     game.socket.emit("module.npc-chatter", {
-      tokenId: token.data._id,
+      tokenId: token.id,
       msg: result
     });
+
     const emote = Object.keys(options).length ? {emote: options} : false;
     await canvas.hud.bubbles.say(token.data, result, emote);
   }
-
   async selectedChatter(options={}) {
     const npcTokens = canvas.tokens.controlled;
-    const tokenIndex = Math.floor((Math.random() * npcTokens.length) + 0);
-    const token = npcTokens[tokenIndex];
+    const tokenIndex = Math.floor((Math.random() * npcTokens.length));
+    const token = canvas.tokens.controlled[tokenIndex];
     return this.tokenChatter(token, options);
   }
   
@@ -98,7 +122,7 @@ class NpcChatter {
 
 Hooks.once('init', async () => {
   game.settings.register("npc-chatter", "scenetype", {
-    name: "Should Tokens Chatter on the active scene, or the viewed scene?",
+    name: "Should Tokens chatter on the active scene, or the viewed scene?",
     type: String,
     config: true,
     scope: "world",
@@ -114,9 +138,7 @@ Hooks.once('ready', async () => {
   game.npcChatter = new NpcChatter();
 
   game.socket.on("module.npc-chatter", async (toShow) => {
-    //console.log("Got token " + toShow.tokenId + " with text " + toShow.msg);
     let token = canvas.tokens.get(toShow.tokenId);
-    //console.log(token);
     canvas.hud.bubbles.say(token, toShow.msg, false);
   });
 });
